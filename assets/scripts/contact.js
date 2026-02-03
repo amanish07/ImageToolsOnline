072153a638e469b1de6dcbd3f13d7e1fb9714e49
amanish07 <amanraj05100@gmail.com>
2026-02-03 21:54:47 +0530

    js

---
+++ assets/scripts/contact.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navMenu.style.display =
+    navMenu.style.display === "flex" ? "none" : "flex";
+});
